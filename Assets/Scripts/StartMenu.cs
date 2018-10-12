d428a8f356eeb1890b2c64f9c9ff3766d4a2d209
Monkeychipz <34406624+Monkeychipz@users.noreply.github.com>
2018-10-12 15:30:56 -0700

    Assets and Project Settings

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+    public string ctr_num;
+	// Use this for initialization
+	void Update () {
+        ChangeScenes("Game");
+	}
+    public void ChangeScenes(string scene)
+    {
+        if (Input.GetButtonDown(ctr_num + " A Button"))
+            SceneManager.LoadScene(scene);
+    }
+}
