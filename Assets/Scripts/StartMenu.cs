d934b4c4112988411b887e3cd7cbe8b45ef5c286
Monkeychipz <34406624+Monkeychipz@users.noreply.github.com>
2018-10-13 17:24:00 -0700

    should be able to switch between scenes

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -5,13 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour {
     public string ctr_num;
+    public string next_scene;
 	// Use this for initialization
 	void Update () {
-        ChangeScenes("Game");
+        ChangeScenes(next_scene);
+        Exitgame();
 	}
     public void ChangeScenes(string scene)
     {
-        if (Input.GetButtonDown(ctr_num + " A Button"))
+       //if(Input.GetButtonDown(ctr_num + " A Button"))
+        if (Input.GetMouseButtonDown(0))
             SceneManager.LoadScene(scene);
+
+    }
+    public void Exitgame()
+    {
+        //if(Input.GetButtonDown(ctr_num + " A Button"))
+        if (Input.GetMouseButtonDown(0))
+            Application.Quit();
     }
 }
