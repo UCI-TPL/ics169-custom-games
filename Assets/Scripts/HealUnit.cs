c2d123da74817178f09379aa9339907a7a4d2ab9
Monkeychipz <34406624+Monkeychipz@users.noreply.github.com>
2018-10-18 13:38:05 -0700

    BaseScript

---
+++ Assets/Scripts/HealUnit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealUnit : BaseScript {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void HealMember(int heal)
+    {
+        if (current_health + heal > health)
+            current_health = health;
+        else
+            current_health += heal;
+
+        int result = attack / check_dmg;
+        if (current_attack + result > attack)
+            current_attack = attack;
+        current_attack += result * attack_loss;
+    }
+}
