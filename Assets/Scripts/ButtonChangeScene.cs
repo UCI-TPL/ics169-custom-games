c9268d7560188a3edafc71c48a5119c9b3f23edd
duncanwycliffe <dwycliff@uci.edu>
2018-10-17 18:07:43 -0700

    Added main menu and victory screen
    victory screen can lead back to main menu and exit game buttons work
    going to add sounds

---
+++ Assets/Scripts/ButtonChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonChangeScene : MonoBehaviour 
+{
+
+	public Button b;
+	public string name;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Button b1 = b.GetComponent<Button>();
+		b1.onClick.AddListener(TaskOnClick);
+	}
+	
+	void TaskOnClick()
+	{
+		SceneManager.LoadScene(name);	
+	}
+}
