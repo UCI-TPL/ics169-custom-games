072a31c59e78a23a3824b0687769864c9d4589d7
redderpanda <jaredsc@uci.edu>
2018-10-26 16:55:32 -0700

    pool working for draft

--- Assets/Scripts/PlayerInformation.cs
+++ Assets/Scripts/PlayerInformation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerInformation : MonoBehaviour
 {
@@ -13,12 +14,16 @@ public class PlayerInformation : MonoBehaviour
     public string player1; // player 1 string to map to the correct input
     public string player2; // player 2 string to map to the correct input
 
+    public Text display;
+
     public Scene current_scene;
 
     public bool plr1Set, plr2Set;
     //___________________________________________________________________________________________
     public List<StartUnit> AllP1Units = new List<StartUnit>(); // p2 can share this list but just change their tag later
     public List<StartUnit> AllP2Units = new List<StartUnit>();
+
+    public List<StartUnit> PoolUnits = new List<StartUnit>(); // random  pool of units
     public float p1ScrollTime;
     public float p2ScrollTime;
     public int p1ScrollValue;
@@ -26,12 +31,19 @@ public class PlayerInformation : MonoBehaviour
 
     public float p1PickTime;
 
+    public bool pool = false;
+
 
     // Use this for initialization
     void Start()
     {
+        display = FindObjectOfType<Text>();
         DontDestroyOnLoad(this.gameObject);
         current_scene = SceneManager.GetActiveScene();
+        if(pool)
+        {
+            RandomPool();
+        }
     }
 
     // Update is called once per frame
@@ -57,6 +69,27 @@ public class PlayerInformation : MonoBehaviour
         }
     }
 
+    public void RandomPool()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            int rand_index = Random.Range(0, AllP1Units.Count);
+            PoolUnits.Add(AllP1Units[rand_index]);
+        }
+    }
+
+    public void CheckUnits()
+    {
+        for(int i = 0; i < AllP1Units.Count-1; i++)
+        {
+            if(!PoolUnits.Contains(AllP1Units[i]))
+            {
+                AllP1Units.Remove(AllP1Units[i]);
+            }
+                
+        }
+    }
+
 
     void ControllerMapping()
     {
@@ -64,12 +97,14 @@ public class PlayerInformation : MonoBehaviour
         {
             if (Input.GetButtonDown("J1 A Button") && !ctrsSet.Contains(1))
             {
+                display.text += "PLAYER 1 READY\n";
                 plr1Set = true;
                 player1 = "J1 ";
                 ctrsSet.Add(1);
             }
             else if (Input.GetButtonDown("J2 A Button") && !ctrsSet.Contains(2))
             {
+                display.text += "PLAYER 2 READY\n";
                 plr1Set = true;
                 player1 = "J2 ";
                 ctrsSet.Add(2);
@@ -79,12 +114,14 @@ public class PlayerInformation : MonoBehaviour
         {
             if (Input.GetButtonDown("J1 A Button") && !ctrsSet.Contains(1))
             {
+                display.text += "PLAYER 1 READY\n";
                 plr2Set = true;
                 player2 = "J1 ";
                 ctrsSet.Add(1);
             }
             else if (Input.GetButtonDown("J2 A Button") && !ctrsSet.Contains(2))
             {
+                display.text += "PLAYER 2 READY\n";
                 plr2Set = true;
                 player2 = "J2 ";
                 ctrsSet.Add(2);
@@ -144,6 +181,7 @@ public class PlayerInformation : MonoBehaviour
                 }
             }
         }
+
     }
 
     private void DraftPick()
@@ -155,6 +193,11 @@ public class PlayerInformation : MonoBehaviour
             {
                 p1PickTime = Time.time + 1f;
                 Player1Chosen.Add(AllP1Units[p1ScrollValue]);
+                if (pool)
+                {
+                    PoolUnits.Remove(AllP1Units[p1ScrollValue]);
+                    CheckUnits();
+                }
             }
         }
         //if (Player2Chosen.Count < 3)
