16a47188f2766b1458f9e1c61827af68029f4710
ChristopheSaucier <csaucier27@gmail.com>
2018-11-05 13:38:20 -0800

    Fixed All Merge Issues

--- Assets/Scripts/StartUnit.cs
+++ Assets/Scripts/StartUnit.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StartUnit : MonoBehaviour {
+public class StartUnit : MonoBehaviour
+{
 
     public string unit_type;
     public string unit_name;
@@ -13,12 +14,10 @@ public class StartUnit : MonoBehaviour {
     public int attack;
     public int basedmg;
     public float crit;
-
-    public int cost;
     public float miss;
     public float crit_multiplier;
     public Sprite Icon;
-
+    public int cost;
     //public int attack_loss; // how much attack a unit loses when hit
     //public int check_dmg; // check if dmg is greater than this amount to know if you lower the dmg or not
     public float current_health;
@@ -28,6 +27,7 @@ public class StartUnit : MonoBehaviour {
     public GameObject health_bar;
     Animator anim;
     public HexagonMapEditor editor;
+    public GameObject Unit_Stats_Panel;
 
 
     //Attack, Hit, and Move sounds
@@ -35,31 +35,30 @@ public class StartUnit : MonoBehaviour {
 
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         editor = FindObjectOfType<HexagonMapEditor>();
-        attackSound = GetComponent<AudioSource>();
-        hitSound = GetComponent<AudioSource>();
-        moveSound = GetComponent<AudioSource>();
-        attackSound.playOnAwake = false;
-        hitSound.playOnAwake = false;
-        moveSound.playOnAwake = false;
-
         anim = GetComponent<Animator>();
         current_health = health;
         current_attack = attack;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if(dead)
-            StartCoroutine(Dead());
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dead)
+        {
+            //StartCoroutine(Dead());
+            this.gameObject.SetActive(false);
+        }
+
+    }
 
     IEnumerator Dead()
     {
         yield return new WaitForSeconds(0.5f);
-        Destroy(this.gameObject);   
+        Destroy(this.gameObject);
     }
 
     public IEnumerator BasicAttack(Grid hexGrid, HexagonCell unitCell) // return bool yes if dead false if no
@@ -91,17 +90,16 @@ public class StartUnit : MonoBehaviour {
             targetable[rand_index].unitOnTile.current_health -= damage;
             attacked_unit.health_bar.GetComponent<Image>().fillAmount = attacked_unit.current_health / attacked_unit.health; // fix?
 
-            //if (targetable[rand_index].unitOnTile.current_attack > 10)
-            //{
-            float percenthealth = targetable[rand_index].unitOnTile.current_health / targetable[rand_index].unitOnTile.health;
-            if(targetable[rand_index].unitOnTile.current_attack * percenthealth > 10)
+            if (targetable[rand_index].unitOnTile.current_attack > 10)
+            {
+                float percenthealth = targetable[rand_index].unitOnTile.current_health / targetable[rand_index].unitOnTile.health;
                 targetable[rand_index].unitOnTile.current_attack *= percenthealth;
-            //}
+            }
 
             Debug.Log("he dead");
             if (targetable[rand_index].unitOnTile.current_health <= 0)
             {
-                
+
                 int index = targetable[rand_index].coords.X_coord + targetable[rand_index].coords.Z_coord * hexGrid.width + targetable[rand_index].coords.Z_coord / 2;
                 editor.RemoveUnitInfo(targetable[rand_index], index);
             }
@@ -117,7 +115,7 @@ public class StartUnit : MonoBehaviour {
     public IEnumerator Attack()
     {
         anim.SetBool("Attacking", true);
-        attackSound.Play();
+        //attackSound.Play();
         yield return new WaitForSeconds(0.5f);
         anim.SetBool("Attacking", false);
 
@@ -125,7 +123,7 @@ public class StartUnit : MonoBehaviour {
     public IEnumerator Hit()
     {
         anim.SetBool("Hurt", true);
-        hitSound.Play();
+        //hitSound.Play();
         yield return new WaitForSeconds(0.4f);
         anim.SetBool("Hurt", false);
     }
@@ -134,7 +132,7 @@ public class StartUnit : MonoBehaviour {
     {
         //Debug.Log("moving");
         anim.SetBool("Moving", true);
-        moveSound.Play(); 
+        //moveSound.Play(); 
         yield return new WaitForSeconds(0.4f);
         anim.SetBool("Moving", false);
     }
