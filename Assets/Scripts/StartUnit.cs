9acbd34f761af31df45c090e92c6ccd55722e831
Monkeychipz <34406624+Monkeychipz@users.noreply.github.com>
2018-11-06 17:21:28 -0800

    Prefabs and implemented miss into start unit

--- Assets/Scripts/StartUnit.cs
+++ Assets/Scripts/StartUnit.cs
@@ -76,16 +76,32 @@ public class StartUnit : MonoBehaviour
         {
             StartCoroutine(Attack());
             int rand_index = Random.Range(0, targetable.Count);
-            float random_val = Random.value;
+            float crit_chance = Random.value;
+            float miss_chance = Random.value;
             float damage = current_attack;
-            if (random_val < crit)
-                damage = current_attack * 2;
-            int dmg_txt = (int)damage;
+            int dmg_txt = 0;
+
+            if (miss_chance < miss)
+                damage = 0;
+
+            if (miss != 0)
+            {
+                if (crit_chance < crit)
+                    damage = current_attack * crit_multiplier;
+                dmg_txt = (int)damage;
+            }
+
             if (targetable[rand_index].unitOnTile.FloatingTextPrefab)
             {
                 GameObject damagetext = Instantiate(targetable[rand_index].unitOnTile.FloatingTextPrefab, targetable[rand_index].unitOnTile.transform.position, Quaternion.identity, transform);
-                damagetext.GetComponent<TextMesh>().text = dmg_txt.ToString();
+                if (damage == 0)
+                    damagetext.GetComponent<TextMesh>().text = "MISS";
+                if(damage != 0)
+                    damagetext.GetComponent<TextMesh>().text = dmg_txt.ToString();
+                if (damagetext.transform.localScale.x == -1)
+                    damagetext.gameObject.transform.localScale = new Vector3(1,0,0);
             }
+
             StartUnit attacked_unit = targetable[rand_index].unitOnTile;
             targetable[rand_index].unitOnTile.current_health -= damage;
             attacked_unit.health_bar.GetComponent<Image>().fillAmount = attacked_unit.current_health / attacked_unit.health; // fix?
--- Assets/Scripts/TeamPowerupTiles.cs
+++ Assets/Scripts/TeamPowerupTiles.cs
@@ -3,59 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TeamPowerupTiles : MonoBehaviour {
-    public bool is_occupied,attackBuff,healthBuff,mobilityBuff,critBuff,attackrangeBuff;
+    public bool discovered,attackBuff,healthBuff,mobilityBuff,critBuff,attackrangeBuff,waterDebuff,grassDebuff;
     public List<StartUnit> UnitsTeam;
+    public StartUnit UnitonTile;
+    public int mobility, debuff;
     // Use this for initialization
     void Start () {
-        is_occupied = false;
+        discovered = false;
+        mobility = UnitonTile.mobility;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Debuff();
         Buff();
 	}
 
     void Buff()
     {
-        if (is_occupied == true)
+        if (discovered == true)
         {
-            if (attackBuff)
+            for(int i = 0; i < UnitsTeam.Count; i++)
             {
-                for (int i = 0; i < UnitsTeam.Count; i++)
-                {
+                if (attackBuff)
                     UnitsTeam[i].attack += 20;
-                }
-            }
-            if (healthBuff)
-            {
-                for (int i = 0; i < UnitsTeam.Count; i++)
-                {
+                if (healthBuff)
                     UnitsTeam[i].health += 100;
-                }
-            }
-            if (mobilityBuff)
-            {
-                for (int i = 0; i < UnitsTeam.Count; i++)
-                {
+                if (mobilityBuff)
                     UnitsTeam[i].mobility += 1;
-                }
-            }
-            if (critBuff)
-            {
-                for (int i = 0; i < UnitsTeam.Count; i++)
-                {
+                if (critBuff)
                     UnitsTeam[i].crit += .2f;
-                }
-            }
-            if (attackrangeBuff)
-            {
-                for (int i = 0; i < UnitsTeam.Count; i++)
-                {
+                if (attackrangeBuff)
                     UnitsTeam[i].attackRange += 1;
-                }
             }
+            this.gameObject.tag = "Floor";
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = this.gameObject.GetComponent<Grid>().Floor;
             this.gameObject.GetComponent<TeamPowerupTiles>().enabled = !this.gameObject.GetComponent<TeamPowerupTiles>().enabled;
         }
-        is_occupied = false;
+        //is_occupied = false;
+    }
+
+    void Debuff()
+    {
+        if (discovered && UnitonTile != null)
+        {
+            if (grassDebuff)
+            {
+                if (UnitonTile.mobility - 1 < 1)
+                    UnitonTile.mobility = 1;
+                else
+                    UnitonTile.mobility = mobility - 1;
+                discovered = false;
+            }
+
+            if (waterDebuff)
+            {
+                if (UnitonTile.mobility - 2 < 0)
+                    UnitonTile.mobility = 1;
+                else
+                    UnitonTile.mobility = mobility - 2;
+                discovered = false;
+            }
+        }
+
     }
 }
