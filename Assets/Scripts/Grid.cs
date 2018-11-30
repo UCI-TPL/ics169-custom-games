24e248c469ad13130fa6a450334b53782a636706
redderpanda <jaredsc@uci.edu>
2018-11-29 16:38:53 -0800

    update

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -215,7 +215,7 @@ public class Grid : MonoBehaviour {
 
     public HexagonCell[] CreateGrid()
     {
-        ten = true;
+        //ten = true;
         if (ten)
         {
             height = 10;
@@ -236,25 +236,25 @@ public class Grid : MonoBehaviour {
         if (twenty)
         {
             height = 20;
-        width = 20;
+            width = 20;
         //int randmap = Random.Range(0, 2);
         //if (randmap == 0)
         //{
-        height = 10;
-        width = 10;
-        cells = new HexagonCell[height * width]; // create an array of correct length
+        //height = 10;
+        //width = 10;
+            cells = new HexagonCell[height * width]; // create an array of correct length
 
-        for (int b = 0, c = 0; b < height; b++) // fill the array with actual hexagon cells
-        {
-
-            for (int a = 0; a < width; a++)
+            for (int b = 0, c = 0; b < height; b++) // fill the array with actual hexagon cells
             {
-                CreateCell(a, b, c++);
+
+                for (int a = 0; a < width; a++)
+                {
+                    CreateCell(a, b, c++);
+                }
             }
+            result = ChangeHexInfo(cells, wall_list2, powerlist2, hazards2, water2);
         }
-        result = ChangeHexInfo(cells, wall_list2, powerlist2, hazards2, water2);
-        }
-        result = ChangeHexInfo(cells, wall_list1, powerlist1, hazards, water);
+        //result = ChangeHexInfo(cells, wall_list1, powerlist1, hazards, water);
         //}
 
         //if(randmap == 1)
