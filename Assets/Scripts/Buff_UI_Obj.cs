0537e6a6b2d07cccaa8d7b02a5a9c66e29560c5b
ChristopheSaucier <csaucier27@gmail.com>
2019-02-10 15:35:41 -0800

    Unit Buff Manager Added

---
+++ Assets/Scripts/Buff_UI_Obj.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff_UI_Obj : MonoBehaviour {
+    public GameObject Icon_Obj;
+    public string Buff_ID;
+}
